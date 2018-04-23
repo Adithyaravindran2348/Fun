1a925cbf253cbff529c8c791c9613a61f51e5916
Adithyaravindran2348 <Adithya.Ravindran.2348@student.uu.se>
2018-04-24 00:07:30 +0200

    Add files via upload

---
+++ Searchengine/Books.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Search_application.Models
+{
+    public class Books
+    {
+
+        //five different attributes of book entities
+        public string author { get; set; }
+        public string title { get; set; }
+
+        public string price { get; set; }
+        public string publish_date { get; set; }
+        public string description { get; set; }
+
+
+    }
+
+   }
